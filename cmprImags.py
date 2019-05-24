10f5680c3cbfebe49d478016268c1f4ecfe467ba
VishnuJin <artharpain143@gmail.com>
2019-05-24 17:43:15 +0000

    adding same image comparision

--- cmprImags.py
+++ cmprImags.py
@@ -3,12 +3,17 @@
 import cv2
 
 org = cv2.imread("imgOut\prototype1.jpg")
-tst = cv2.imread("imgOut\sample1.jpg")
+#tst = cv2.imread("imgOut\sample1.jpg")
+dup = cv2.imread("imgOut\prototype - duplicate1.jpg")
 
-if org.shape == tst.shape:
+if org.shape == dup.shape:
     print("Images size matches")
-    diff = cv2.subtract(org,tst)
-
+    print(org.shape)
+    print(dup.shape)
+    diff = cv2.subtract(org,dup)
+    r,g,b = cv2.split(diff)
+    if (cv2.countNonZero(r)==0) & (cv2.countNonZero(g)==0) & (cv2.countNonZero(b)==0):
+        print("The images are comletely equal")
     cv2.imshow('compared',diff)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
